6020e99c524c3ab21b5df73afc4b8f4cd4ca69e7
CMecker <cb82zywi@studserv.uni-leipzig.de>
2019-10-08 00:03:25 +0200

    Added walls

---
+++ jellytest/jellytest/Scenes/Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+const GRAVITY = 20
+const SPEED = 30
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	var move = Vector2(0,GRAVITY)
+	if Input.is_action_pressed("left"):
+			move.x = - SPEED
+	if Input.is_action_pressed("right"):
+			move.x = + SPEED
+	if Input.is_action_pressed("up"):
+			move.y = - SPEED
+	if Input.is_action_pressed("down"):
+			move.y = + SPEED
+		
+	move_and_slide(move)
