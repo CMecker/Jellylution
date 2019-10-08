fccd9d3437d8b49d93342bffdda512271e2d43ba
CMecker <cb82zywi@studserv.uni-leipzig.de>
2019-10-08 23:29:20 +0200

    Implemented movement

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -5,21 +5,49 @@ extends KinematicBody2D
 # var b = "text"
 const GRAVITY = 20
 const SPEED = 30
+const JUMP_POWER=50
+const UP_VECTOR = Vector2(0,-1)
+var movement = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	var move = Vector2(0,GRAVITY)
-	if Input.is_action_pressed("left"):
-			move.x = - SPEED
-	if Input.is_action_pressed("right"):
-			move.x = + SPEED
-	if Input.is_action_pressed("up"):
-			move.y = - SPEED
-	if Input.is_action_pressed("down"):
-			move.y = + SPEED
-		
-	move_and_slide(move)
+	movement.x = 0
+	
+	if is_on_ceiling() or is_on_floor():
+		movement.y = 0 
+	
+	movement.y += GRAVITY * delta
+
+	check_key_input()
+
+	move_and_slide(movement, UP_VECTOR)
+	
+	set_animation()
 	
 	$AnimationPlayer.play("idle")
+	
+func check_key_input():
+	if Input.is_action_pressed("left"):
+		movement.x = -1 * SPEED
+	if Input.is_action_pressed("right"):
+		movement.x = 1 * SPEED
+	if Input.is_action_pressed("jump") and is_on_floor():
+		movement.y = -JUMP_POWER
+		
+func set_animation():
+	if movement.x < 0:
+		$AnimationPlayer.play("go")
+		$moves.flip_h=true
+	if movement.x > 0:
+		$AnimationPlayer.play("go")
+		$moves.flip_h= false
+#	if Input.is_action_pressed("left"):
+#		move.x = - SPEED
+#	if Input.is_action_pressed("right"):
+#		move.x = + SPEED
+#	if Input.is_action_pressed("up"):
+#		move.y = - SPEED
+#	if Input.is_action_pressed("down"):
+#		move.y = + SPEED
